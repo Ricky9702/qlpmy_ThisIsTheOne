b87b9d695aa6d622f158d776bdd11d6189d900b3
Ricky9702 <116155688+Ricky9702@users.noreply.github.com>
2022-12-15 22:26:06 +0700

    update function show all patient medical reports by date

--- QLPMT/dao.py
+++ QLPMT/dao.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func, asc
+from sqlalchemy import func, asc, delete
 
 from QLPMT.models import User, BenhNhan, DanhSachKham, UserRole, PhieuKhamBenh, ChiTietPhieuKhamBenh, \
     LoaiThuoc, Thuoc, DonVi, BacSi, HoaDon
@@ -10,14 +10,17 @@ def online_register(HoTen, GioiTinh, NamSinh, DiaChi, DanhSachKham_id):
     b = BenhNhan(HoTen=HoTen, GioiTinh=GioiTinh, NamSinh=NamSinh, DiaChi=DiaChi, DanhSachKham_id=DanhSachKham_id)
     db.session.add(b)
     db.session.commit()
-# Phieu kham benh
-def add_medical_report(ngaykham, mabenhnhan, trieuchung, dudoanbenh):
 
+
+# Phieu kham benhbe
+def add_medical_report(ngaykham, mabenhnhan, trieuchung, dudoanbenh):
     pk = PhieuKhamBenh(NgayKham=ngaykham, TrieuChung=trieuchung, DuDoanBenh=dudoanbenh, BacSi_id=1,
                        BenhNhan_id=mabenhnhan, HoaDon_id=1)
     db.session.add(pk)
     db.session.commit()
-def add_detial_medical_report(tenloaithuoc, tenthuoc, soluong, cachdung):
+
+
+def add_detail_medical_report(tenloaithuoc, tenthuoc, soluong, cachdung):
     thuoc_id = get_med_id(tenthuoc, tenloaithuoc)
     phieukham_id = (get_medical_report_last_id())
     ctpkb = ChiTietPhieuKhamBenh(SoLuong=soluong, CachDung=cachdung, PhieuKhamBenh_id=phieukham_id,
@@ -28,7 +31,7 @@ def add_detial_medical_report(tenloaithuoc, tenthuoc, soluong, cachdung):
 
 def load_med_name(med_type=None):
     if med_type:
-        return Thuoc.query.\
+        return Thuoc.query. \
             filter(Thuoc.LoaiThuoc_id == LoaiThuoc.id).filter(LoaiThuoc.TenLoaiThuoc == med_type).all()
     return Thuoc.query.all()
 
@@ -55,23 +58,24 @@ def get_med_id(med_name=None, med_type=None):
 
 
 def get_medical_date_of_patient(patient_id):
-    return db.session.query(ChiTietPhieuKhamBenh, PhieuKhamBenh, BenhNhan). \
-        join(PhieuKhamBenh, ChiTietPhieuKhamBenh.PhieuKhamBenh_id.__eq__(PhieuKhamBenh.id)). \
+    return db.session.query(PhieuKhamBenh, BenhNhan). \
         join(BenhNhan, PhieuKhamBenh.BenhNhan_id.__eq__(BenhNhan.id)). \
         filter(BenhNhan.id.__eq__(patient_id)).all()
-def load_all_medical_report(patient_id, med_date):
-    return db.session.query(ChiTietPhieuKhamBenh, PhieuKhamBenh, BenhNhan, Thuoc, LoaiThuoc, DonVi). \
-        join(PhieuKhamBenh, ChiTietPhieuKhamBenh.PhieuKhamBenh_id.__eq__(PhieuKhamBenh.id), isouter=True). \
-        join(BenhNhan, PhieuKhamBenh.BenhNhan_id.__eq__(BenhNhan.id), isouter=True). \
-        join(Thuoc, ChiTietPhieuKhamBenh.Thuoc_id.__eq__(Thuoc.id), isouter=True).\
-        join(DonVi, Thuoc.DonVi_id.__eq__(DonVi.id), isouter=True).\
-        join(LoaiThuoc, Thuoc.LoaiThuoc_id.__eq__(LoaiThuoc.id), isouter=True).\
-        filter(BenhNhan.id.__eq__(patient_id) and PhieuKhamBenh.NgayKham.strftime("%d%m%Y") == med_date).all()
+
+
+def load_detail_medical_report(med_report_id):
+    return db.session.query(ChiTietPhieuKhamBenh, PhieuKhamBenh).\
+        join(PhieuKhamBenh, ChiTietPhieuKhamBenh.PhieuKhamBenh_id.__eq__(PhieuKhamBenh.id)).\
+        filter(PhieuKhamBenh.id == med_report_id).all()
+
+
 def some_test():
     return db.session.query()
 
+
 def get_medical_report_last_id():
     return db.session.query(func.max(PhieuKhamBenh.id)).scalar()
+
 
 def get_payment_last_id():
     return db.session.query(func.max(HoaDon.id)).scalar()
@@ -112,7 +116,7 @@ def load_BenhNhan():
 def auth_user(username, password):
     password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
     return User.query.filter(User.username.__eq__(username.strip()),
-                                  User.password.__eq__(password)).first()
+                             User.password.__eq__(password)).first()
 
 
 def get_user_by_id(user_id):
--- QLPMT/index.py
+++ QLPMT/index.py
@@ -31,7 +31,9 @@ def online_register():
 @app.route('/medical-report', methods=['get', 'post'])
 def medical_report():
     key = app.config['MEDICAL_REPORT_KEY']
-    med_report = session.get(key)
+    key2 = app.config['MEDICAL_REPORT_MEDICINE_KEY']
+    medical_reports = session.get(key)
+    medical_report_medicine = session.get(key2)
     med_type = dao.load_med_type()
     med_info = dao.load_med_info()
     med_name = dao.load_med_name()
@@ -48,18 +50,20 @@ def medical_report():
     #     render_template('index.html')
     if request.method.__eq__('POST'):
         try:
-            for k in med_report:
-                data = med_report[k]
-                dao.add_medical_report(ngaykham=data.get('report_date'),
-                                       mabenhnhan=int(data.get('patient_id')),
-                                       trieuchung=data.get('symptoms'),
-                                       dudoanbenh=data.get('diagnose'))
-                dao.add_detial_medical_report(tenloaithuoc=data.get('med_type'),
+            dao.add_medical_report(ngaykham=medical_reports.get('report_date'),
+                                   mabenhnhan=int(medical_reports.get('patient_id')),
+                                   trieuchung=medical_reports.get('symptoms'),
+                                   dudoanbenh=medical_reports.get('diagnose'))
+            for key in medical_report_medicine.keys():
+                data = medical_report_medicine[key]
+                dao.add_detail_medical_report(tenloaithuoc=data.get('med_type'),
                                               tenthuoc=data.get('med_name'),
                                               soluong=int(data.get('med_quantity')),
                                               cachdung=data.get('med_usage'))
                 dao.update_med_amount(med_name=data.get('med_name'), amount=data.get('med_quantity'))
-                err_msg = "Lưu phiếu khám bệnh thành công!!!"
+
+            err_msg = "Lưu phiếu khám bệnh thành công!!!"
+            redirect('/medical-report')
         except:
             err_msg = 'Hệ thống đang có lỗi! Vui lòng quay lại sau!'
     return render_template('medical_report.html', med_info=med_info, med_type=med_type, med_name=med_name,
@@ -72,11 +76,9 @@ def load_med_name_by_type():
     med_name = session.get(key)
     data = request.json
     med_type = data['med_type']
-    print(med_type)
+    temp = dao.load_med_name(med_type=med_type)
     result = []
-    filter = dao.load_med_name(med_type=med_type)
-    print(filter)
-    for item in filter:
+    for item in temp:
         result.append({
             'med_name': item.TenThuoc
         })
@@ -88,102 +90,105 @@ def load_med_name_by_type():
 
 
 @app.route('/api/save-med-report', methods=['post'])
-def load_save_med_report():
+def load_save_medical_report():
     key = app.config['MEDICAL_REPORT_SAVE_KEY']
     data = request.json
-    report_date = data['report_date']
-    patient_id = data['patient_id']
-    symptoms = data['symptoms']
-    diagnose = data['diagnose']
-    med_name = data['med_name']
-    med_type = data['med_type']
-    med_quantity = data['med_quantity']
-    med_usage = data['med_usage']
     not_included_med = data['not_included_med']
     if not_included_med == 'on':
         not_included_med = '0'
-    med_report_save = {
-        "report_date": report_date,
-        "patient_id": patient_id,
-        "symptoms": symptoms,
-        "diagnose": diagnose,
-        "med_name": med_name,
-        "med_type": med_type,
-        "med_quantity": med_quantity,
-        "med_usage": med_usage,
+    medical_report_save = {
+        "report_date": data['report_date'],
+        "patient_id": data['patient_id'],
+        "symptoms": data['symptoms'],
+        "diagnose": data['diagnose'],
+        "med_name": data['med_name'],
+        "med_type": data['med_type'],
+        "med_quantity": data['med_quantity'],
+        "med_usage": data['med_usage'],
         "not_included_med": not_included_med
     }
-    med_report_save = {k: v for k, v in med_report_save.items() if v is not None}
-    session[key] = med_report_save
-    res = make_response(jsonify(med_report_save), 200)
+    medical_report_save = {k: v for k, v in medical_report_save.items() if v is not None}
+    session[key] = medical_report_save
+    res = make_response(jsonify(medical_report_save), 200)
     return res
 
 
 @app.route('/api/load-patient-med-report', methods=['post'])
-def load_med_report():
+def load_medical_report():
     data = request.json
     patient_id = data['patient_id']
-    temp_list = []
-    filter = dao.get_medical_date_of_patient(patient_id=patient_id)
-    for id in range((len(filter))):
-        for item in filter:
-            temp_list.append({
-
+    temp = dao.get_medical_date_of_patient(patient_id=patient_id)
+    result = []
+    added = set()
+    for item in temp:
+        if item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y") not in added:
+            result.append({
                 'med_date': item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y"),
-                'patient_name': item.BenhNhan.HoTen
+                'patient_name': item.BenhNhan.HoTen,
+                'patient_id': item.BenhNhan.id
             })
-    res = []
-    [res.append(x) for x in temp_list if x not in res]
-    res = make_response(jsonify(res), 200)
+            added.add(item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y"))
+    print(result)
+    res = make_response(jsonify(result), 200)
     return res
 
 
 @app.route('/api/show-patient-med-report', methods=['post'])
-def show_patient_med_report_by_date():
+def show_patient_medical_report_by_date():
     data = request.json
     patient_id = data['patient_id']
     med_date = data['med_date']
-    print(patient_id)
-    print("Ngay:", med_date)
-    temp_list = []
-    filter = dao.load_all_medical_report(patient_id=patient_id, med_date=med_date)
-    print(filter)
-    print(len(filter))
-    for id in range((len(filter))):
-        for item in filter:
-            if item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y") == med_date:
-                if not isinstance(item.Thuoc, type(None)):
-                    temp_list.append({
-                        "med_date": med_date,
+    temp = dao.get_medical_date_of_patient(patient_id=patient_id)
+    result = []
+    for item in temp:
+        if item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y") == med_date:
+            dt = dao.load_detail_medical_report(item.PhieuKhamBenh.id)
+            if len(dt) > 0:
+                for d in dt:
+                    if dt.index(d) == 0:
+                        result.append({
+                            "medical_report": {
+                                "med_date": d.ChiTietPhieuKhamBenh.phieukhambenh.NgayKham.strftime("%d/%m/%Y"),
+                                "symptoms": d.ChiTietPhieuKhamBenh.phieukhambenh.TrieuChung,
+                                "diagnose": d.ChiTietPhieuKhamBenh.phieukhambenh.DuDoanBenh,
+                                "med_included": 1
+                            },
+                            "medicine": {
+                                "med_name": d.ChiTietPhieuKhamBenh.thuoc.TenThuoc,
+                                "med_type": d.ChiTietPhieuKhamBenh.thuoc.loaithuoc.TenLoaiThuoc,
+                                "med_unit": d.ChiTietPhieuKhamBenh.thuoc.donvi.TenDonVi,
+                                "med_quantity": d.ChiTietPhieuKhamBenh.SoLuong,
+                                "med_usage": d.ChiTietPhieuKhamBenh.CachDung
+                            }
+                        })
+                    else:
+                        result.append({
+                            "medicine": {
+                                "med_name": d.ChiTietPhieuKhamBenh.thuoc.TenThuoc,
+                                "med_type": d.ChiTietPhieuKhamBenh.thuoc.loaithuoc.TenLoaiThuoc,
+                                "med_unit": d.ChiTietPhieuKhamBenh.thuoc.donvi.TenDonVi,
+                                "med_quantity": d.ChiTietPhieuKhamBenh.SoLuong,
+                                "med_usage": d.ChiTietPhieuKhamBenh.CachDung
+                            }
+                        })
+            else:
+                result.append({
+                    "medical_report": {
+                        "med_date": item.PhieuKhamBenh.NgayKham.strftime("%d/%m/%Y"),
                         "symptoms": item.PhieuKhamBenh.TrieuChung,
                         "diagnose": item.PhieuKhamBenh.DuDoanBenh,
-                        "med_name": item.Thuoc.TenThuoc,
-                        "med_type": item.LoaiThuoc.TenLoaiThuoc,
-                        "med_unit": item.DonVi.TenDonVi,
-                        "med_quantity": item.ChiTietPhieuKhamBenh.SoLuong,
-                        "med_usage": item.ChiTietPhieuKhamBenh.CachDung
-                    })
-                else:
-                    temp_list.append({
-                        "med_date": med_date,
-                        "symptoms": item.PhieuKhamBenh.TrieuChung,
-                        "diagnose": item.PhieuKhamBenh.DuDoanBenh
-                    })
-
-    res = []
-    [res.append(x) for x in temp_list if x not in res]
-    print(res)
-    res = make_response(jsonify(res), 200)
+                    }
+                })
+    res = make_response(jsonify(result), 200)
     return res
 
 
 @app.route('/api/clear-med-report')
-def clear_med_report_session():
-    key = app.config['MEDICAL_REPORT_KEY']
-    key2 = app.config['MEDICAL_REPORT_SAVE_KEY']
+def clear_medical_report_session():
     try:
-        del session[key]
-        del session[key2]
+        del session['medical_reports']
+        del session['medical_report_save']
+        del session['medical_report_medicine']
         return jsonify({'status': 204})
     except:
         return jsonify({'status': 404})
@@ -192,85 +197,84 @@ def clear_med_report_session():
 @app.route('/api/add-med-report', methods=['post'])
 def add_med_to_report():
     key = app.config['MEDICAL_REPORT_KEY']
-    med_report = session[key] if key in session else {}
+    key2 = app.config['MEDICAL_REPORT_MEDICINE_KEY']
+    medical_reports = session[key] if key in session else {}
+    medical_report_medicine = session[key2] if key2 in session else {}
     data = request.json
     patient_id = data['patient_id']
     patient_name = dao.get_patient_name(patient_id)
-    report_date = data['report_date']
     med_name = data['med_name']
     med_quantity = data['med_quantity']
-    symptoms = data['symptoms']
-    diagnose = data['diagnose']
-    med_type = data['med_type']
     med_unit = dao.get_med_unit(med_name)
-    med_usage = data['med_usage']
-
-    if patient_name is None:
-        med_report.clear()
-        print(med_report)
-        print(patient_name)
+    if any(d['med_name'] == med_name for d in medical_report_medicine.values()):
+        dict_key = ','.join([str(item) for item in (key for key in medical_report_medicine if
+                                                    med_name in medical_report_medicine[key].values())])
+        result = int(medical_report_medicine[dict_key]["med_quantity"]) + med_quantity
+        medical_report_medicine[dict_key]["med_quantity"] = str(result)
     else:
-        if any(d['patient_id'] != patient_id for d in med_report.values()):
-            med_report.clear()
-        if any(d['patient_id'] == patient_id for d in med_report.values()):
-            if any(d['med_name'] == '' for d in med_report.values()):
-                med_report.clear()
-        if any(d['med_name'] == med_name for d in med_report.values()):
-            dict = ','.join([str(item) for item in (key for key in med_report if med_name in med_report[key].values())])
-            print('phat hien co trung')
-            print(dict)
-            result = int(med_report[dict]["med_quantity"]) + med_quantity
-            med_report[dict]["med_quantity"] = str(result)
+        id = str(len(medical_report_medicine) + 1)
+        print(medical_reports.values())
+        if patient_id not in medical_reports.values():
+            if med_unit is not None:
+                medical_reports = {
+                    "report_date": data['report_date'],
+                    "patient_id": patient_id,
+                    "patient_name": patient_name,
+                    "symptoms": data['symptoms'],
+                    "diagnose": data['diagnose'],
+                }
+                medical_report_medicine[id] = {
+                    "id": id,
+                    "med_name": med_name,
+                    "med_type": data['med_type'],
+                    "med_unit": med_unit,
+                    "med_quantity": med_quantity,
+                    "med_usage": data['med_usage']
+                }
+            else:
+                medical_reports = {
+                    "report_date": data['report_date'],
+                    "patient_id": patient_id,
+                    "patient_name": patient_name,
+                    "symptoms": data['symptoms'],
+                    "diagnose": data['diagnose'],
+                }
         else:
-            id = str(len(med_report) + 1)
-            med_report[id] = {
+            medical_report_medicine[id] = {
                 "id": id,
-                "report_date": report_date,
-                "patient_id": patient_id,
-                "patient_name": patient_name,
-                "symptoms": symptoms,
-                "diagnose": diagnose,
                 "med_name": med_name,
-                "med_type": med_type,
+                "med_type": data['med_type'],
                 "med_unit": med_unit,
                 "med_quantity": med_quantity,
-                "med_usage": med_usage
+                "med_usage": data['med_usage']
             }
-    session[key] = med_report
-    print(med_report)
-    print(len(med_report))
-    # med_report.clear()
-    res = make_response(jsonify(med_report), 200)
+    session[key] = medical_reports
+    session[key2] = medical_report_medicine
+    res = make_response(jsonify(medical_reports), 200)
     return res
 
 
 @app.route('/api/delete-med-report', methods=['delete'])
 def delete_med_in_report():
-    key = app.config['MEDICAL_REPORT_KEY']
-    med_report = session.get(key)
+    key = app.config['MEDICAL_REPORT_MEDICINE_KEY']
+    medical_report_medicine = session.get(key)
     data = request.json
     id = str(data['id'])
     pos = int(id)
-    if med_report and id in med_report:
-        print(med_report.keys())
-        del med_report[id]
+    if medical_report_medicine and id in medical_report_medicine:
+        del medical_report_medicine[id]
         temp_dict = {}
-        for key in list(med_report.keys()):
+        for key in list(medical_report_medicine.keys()):
             if int(key) > pos:
-                print('gia tri cua keys la: ', key)
-                for i in range(pos, len(med_report) + 1):
+                for i in range(pos, len(medical_report_medicine) + 1):
                     new_key = i
-                    print('day la lan thu: ' + str(i) + ' gia tri cua new key: ', new_key)
                     if int(key) > new_key:
                         temp_dict[key] = str(new_key)
-                        print('day la lan thu: ' + str(i) + ' gia tri cua temp dict: ', temp_dict)
-        for old, new in temp_dict.items():
-            print('gia tri cua old: ', old)
-            med_report[new] = med_report.pop(str(old))
-            med_report[new]["id"] = str(new)
-    session[key] = med_report
-    print(med_report)
-    res = make_response(jsonify(med_report), 200)
+    for old, new in temp_dict.items():
+        medical_report_medicine[new] = medical_report_medicine.pop(str(old))
+        medical_report_medicine[new]["id"] = str(new)
+    session[key] = medical_report_medicine
+    res = make_response(jsonify(medical_report_medicine), 200)
     return res
 
 
